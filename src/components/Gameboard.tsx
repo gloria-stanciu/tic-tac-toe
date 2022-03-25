5750b07612279e57fb5baa5d8e5c9193a1a1768e
Gloria Stanciu <gloriastanciu@gmail.com>
2022-03-25 15:55:42 +0200

    Working on conditions

--- src/components/Gameboard.tsx
+++ src/components/Gameboard.tsx
@@ -4,18 +4,26 @@ import Button from "./Button";
 export default function Gameboard(props: { size: number }) {
   const [board, setBoard] = useState<Array<Array<string>>>([[]]);
   const [player, setPlayer] = useState(false); //false is first player and true is second player
-  // const [isEqual, setIsEqual] = useState<Boolean | undefined>()
+  const [gameWon, setGameWon] = useState<Boolean>(false)
+  const [valuePos, setValuePos] = useState<[number, number]>()
 
   function CreateBoard() {
     const newBoard = Array(props.size).fill(new Array(props.size).fill(""));
     setBoard(newBoard);
   }
 
+  function UpdateGame(idRow: number, idCol: number){
+    AddValue(idRow, idCol)
+    CheckLine(idRow)
+    // CheckForWin(idRow, idCol)
+  }
+
   function AddValue(idRow: number, idCol: number) {
     let value = "";
     if (player) value = "0";
     else value = "X";
-
+    
+    setValuePos([idRow, idCol])
     setBoard((prevState) => {
       const newMatrix = prevState.map((arr, idR) => {
         const newRow = arr.map((item, idC) => {
@@ -29,28 +37,54 @@ export default function Gameboard(props: { size: number }) {
       });
       return newMatrix;
     });
+
   }
 
-  function CheckForWin(){
-    let isEqual;
+  function CheckLine(rowId: number){
+    let numberOfX = 0;
+    let numberOf0 = 0;
     for(let i=0; i<board.length; i++){
-      isEqual = true;
-      for(let j=0; j<board.length-1; j++){
-        if(board[i][j] !== board[i][j+1])
-        {
-          isEqual = false;
-        }
+      if(board[rowId][i].toString() === 'X'){
+        numberOfX ++;
+        console.log(numberOfX)
+      }else if(board[rowId][i].toString() === '0'){
+        numberOf0++;
+        console.log(numberOf0)
       }
-      if(isEqual) return isEqual
     }
-    return false
+    if (numberOf0 === board.length || numberOfX === board.length)
+      setGameWon(true)
   }
 
-  function ResetGame(){
+  function CheckColumn(colId: number){
+    let numberOfX = 0;
+    let numberOf0 = 0;
+    for(let i=0; i<board.length; i++){
+      if(board[i][colId].toString() === 'X'){
+        numberOfX ++;
+        console.log(numberOfX)
+      }else if(board[i][colId].toString() === '0'){
+        numberOf0++;
+        console.log(numberOf0)
+      }
+    }
+    if (numberOf0 === board.length-1 || numberOfX === board.length-1)
+      setGameWon(true)
+  }
+
+  function CheckForWin(row:number, column:number) {
+    CheckLine(row)
+    CheckColumn(column)
+    console.log(gameWon);
+    if(gameWon)
+      console.log("am castigaaaat")
+  }
+
+  function ResetGame() {
     setBoard((prevState) => {
       const newMatrix = prevState.map((arr, idR) => {
         const newRow = arr.map((item, idC) => {
-            item = '';
+          item = "";
           return item;
         });
         return newRow;
@@ -58,7 +92,6 @@ export default function Gameboard(props: { size: number }) {
       return newMatrix;
     });
     setPlayer(false);
-
   }
 
   useEffect(() => {
@@ -66,9 +99,10 @@ export default function Gameboard(props: { size: number }) {
   }, []);
 
   useEffect(() => {
-    const res = CheckForWin();
-    console.log(res)
-  }, [player]);
+    if (valuePos)
+      CheckForWin(valuePos?.[0], valuePos?.[1]);
+  }, [valuePos]);
+
 
   return (
     <div className="w-screen flex flex-col justify-center items-center">
@@ -78,15 +112,16 @@ export default function Gameboard(props: { size: number }) {
             <Button
               key={idR.toString() + idC.toString()}
               value={item}
-              onClick={() => AddValue(idR, idC)}
+              onClick={() => UpdateGame(idR, idC)}
             />
           ))}
         </div>
       ))}
-      <button type="reset"
+      <button
+        type="reset"
         className="bg-white font-medium text-gray-500 border-2 border-slate-300 py-4 px-6 rounded-md outline-none mt-8
       hover:bg-slate-300 hover:text-gray-900"
-      onClick={ResetGame}
+        onClick={ResetGame}
       >
         Restart game
       </button>
